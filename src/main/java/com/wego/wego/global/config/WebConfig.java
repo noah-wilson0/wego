78488ba0e4a361cd7b2ef07ff7b0fe1df0d113cd
noah-wilson0 <127368908+noah-wilson0@users.noreply.github.com>
2025-07-17 02:44:10 +0900

    ADD: WEGO-26 CROS 문제 임시 해결
    - 이후 로그인때 security 적용 후 개선할 예정

---
+++ src/main/java/com/wego/wego/global/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.wego.wego.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 URL에 대해
+                .allowedOrigins("http://localhost:5174") // 프론트엔드 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // 쿠키 등 인증정보 허용 시 필요
+    }
+}
